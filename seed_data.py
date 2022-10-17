94eaf245a0c8ad5e470d89959f164721a674892c
aibubich <agusbubich@gmail.com>
2022-10-16 21:57:08 -0300

    se agrego fecha de nacimiento

--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,8 @@
 from familia.models import Familiar
 
-Familiar(nombre="Agustina", direccion="Olazabal 6012", numero_pasaporte=356248967).save()
-Familiar(nombre="Rafael", direccion="Olazabal 6012", numero_pasaporte=28314967).save()
-Familiar(nombre="Delfina", direccion="Olazabal 6012", numero_pasaporte=57203369).save()
-Familiar(nombre="Santiago", direccion="Olazabal 6012", numero_pasaporte=58142367).save()
+Familiar(nombre="Agustina", direccion="Olazabal 6012", numero_pasaporte=356248967,fecha_nacimiento="1990-3-9").save()
+Familiar(nombre="Rafael", direccion="Olazabal 6012", numero_pasaporte=28314967,fecha_nacimiento="1980-2-26").save()
+Familiar(nombre="Delfina", direccion="Olazabal 6012", numero_pasaporte=57203369,fecha_nacimiento="2018-5-27").save()
+Familiar(nombre="Santiago", direccion="Olazabal 6012", numero_pasaporte=58142367, fecha_nacimiento="2021-6-6").save()
 
 print("Se cargo con éxito los usuarios de pruebas")
